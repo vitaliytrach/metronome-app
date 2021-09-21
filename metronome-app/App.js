d82466d6d77c2769a8354dd012dd06b0c72a850b
Vitaliy Trach <vitaliytrach@gmail.com>
2021-09-20 22:00:25 -0700

    Added some started components for the app

--- metronome-app/App.js
+++ metronome-app/App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Set from './components/Set';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Song Sets</Text>
+
+        <View style={styles.items}>
+          {/* This is where the sets go */}
+          <Set text={'09/10/21'} />
+          <Set text={'09/15/21'} />
+          <Set text={'10/08/21'} />
+        </View>
+      </View>
+
     </View>
   );
 }
 
+// Darkest to lightest
+// 1 - #581845
+// 2 - #900c3f
+// 3 - #c70039
+// 4 - #ff5733
+// 5 - #ffc30f
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffc30f'
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {},
 });
---
+++ metronome-app/components/Set.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Set = (props) => {
+
+    return(
+        <View>
+            <Text>{props.text}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Set;
